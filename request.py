0854f7a2e5cd5ccaf2bc8fdae55ea7cf6227732a
srout <swadesna89@gmail.com>
2020-08-02 19:03:45 +0530

    Update request.py

--- request.py
+++ request.py
@@ -7,8 +7,10 @@ import time
 def main():
   # sleep
   time.sleep(1200)
-  res=requests.get('https://raw.githubusercontent.com/jainnikhil30/custom_inv/master/custom.json')
+  res=requests.get('https://raw.githubusercontent.com/SWADESNA/custom_inv/master/custom.json')
   data=res.json()
   print json.dumps(data, sort_keys=True, indent=2)
 if __name__ == '__main__':
   main()
+
+  
